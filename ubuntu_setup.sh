ede672b1dc01fab8ebb8eeed5b27e3550628fb9c
Kenta Konagaya <k.konagaya@kufusha.com>
2022-10-24 16:54:37 +0900

    modify adding yes

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -30,6 +30,6 @@ sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main
 yes | sudo apt install curl # if you haven't already installed curl
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 yes | sudo apt update
-sudo apt install ros-noetic-desktop-full
+yes | sudo apt install ros-noetic-desktop-full
 echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
