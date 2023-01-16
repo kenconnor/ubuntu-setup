65ff68d49ed15e632df8c8b0e978672355b8e4e7
kenconnor <106644673+kenconnor@users.noreply.github.com>
2023-01-16 18:08:43 +0900

    Update ubuntu_setup.sh

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -14,6 +14,7 @@ sudo systemctl start ssh
 # .tmux.confをコピーする
 # ターミナルのプロンプトを短くする
 sed -i -e "s/PS1='\${debian_chroot:+(\$debian_chroot)}"'\\\[\\033\[01;32m\\\]\\u@\\h\\\[\\033\[00m\\\]:\\\[\\033\[01;34m\\\]\\w\\\[\\033\[00m\\\]\\\$ '"'/PS1='\${debian_chroot:+(\$debian_chroot)}"'\\\[\\033\[01;32m\\\]\\u@:\\\[\\033\[01;34m\\\]\\W\\\[\\033\[00m\\\]\\\$ '"'/g" ~/.bashrc
+sed -i -e "s/PS1='"'\${debian_chroot:+(\$debian_chroot)}\\u@\\h:\\w\\\$ '"'/PS1='\$\{debian_chroot:+(\$debian_chroot)}"'\\u@\\W\\\$ '"'/g" ~/.bashrc
 
 # capslock->ctrl
 sed -e 's/XKBOPTIONS=\"\"/XKBOPTIONS=\"ctrl:nocaps\"/g' /etc/default/keyboard | sudo tee /etc/default/keyboard
